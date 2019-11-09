d2dfafbe304c36d8dab2372c65e5d695adaee491
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-11-09 12:02:39 +0530

    Adding Nav

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>e-Portal Home</title>    
+<title>e-Portal Home</title> 
+<link rel="stylesheet" href="css.css">
 </head>
 <body>
+<header id="header-div">
 
+<div id="header-div-menu">
+<ul>
+<a href="#"><li>Home</li></a>
+<a href="#"><li>About Us</li></a>
+<a href="#"><li>Inform Us</li></a>
+<a href="#"><li>Photos</li></a>
+<a href="#"><li>Videos</li></a>
+<a href="#"><li>Contact Us</li></a>
+<a href="#"><li>Login</li></a>
+</ul>    
+</div>    
+
+</header>
 </body>
 </html>
